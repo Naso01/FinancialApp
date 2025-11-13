bae1cb26c5c323c393bdc420fdfd2956ffe5eaaf
Keegan Erdis <erdisk@sheridancollege.ca>
2025-11-13 13:47:12 -0500

    Add project files.

---
+++ InvestmentApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InvestmentApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
