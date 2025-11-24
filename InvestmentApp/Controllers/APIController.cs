0c9624f6f6bb1e905806d693e92de97480acc7ee
Keegan Erdis <erdisk@sheridancollege.ca>
2025-11-24 09:36:08 -0500

    Added API controller

---
+++ InvestmentApp/Controllers/APIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InvestmentApp.Controllers
+{
+    public class APIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
