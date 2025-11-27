0f0f9733fbf58787520f1e3fc176a11cb71554a8
lihan <lihan@DESKTOP-CH8DIB5>
2025-11-27 18:36:03 -0500

    added service classes

---
+++ InvestmentApp/Services/AccountService.cs
@@ -0,0 +1,26 @@
+﻿using InvestmentApp.Models;
+
+namespace InvestmentApp.Services
+{
+    public class AccountService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccountService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool EditAccount(int accountId /* + model later */)
+        {
+            // logic later
+            return true;
+        }
+
+        public bool DeleteAccount(int accountId)
+        {
+            // logic later
+            return true;
+        }
+    }
+}
