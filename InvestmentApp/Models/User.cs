8983beaaa8215975f0ac33ffb91803c168d9a1d9
Nathan Serrano <serranoo@shernet.sheridancollege.ca>
2025-11-27 14:03:41 -0500

    added password to user

--- InvestmentApp/Models/User.cs
+++ InvestmentApp/Models/User.cs
@@ -8,6 +8,8 @@
         public string LastName { get; set; }
         public string Email { get; set; }
 
+        public string Password { get; set; }
+
         // Navigation
         public ChequingAccount ChequingAccount { get; set; }
         public List<Portfolio> Portfolios { get; set; }
